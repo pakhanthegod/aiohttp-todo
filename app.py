701af1c66f3b7a825bf80a4590e0545471c4a94e
Pavel Mametev <pavelmametev@yandex.ru>
2019-04-12 17:53:53 +0500

    Add middleware, authenticate

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from aiohttp.web import Application, run_app
+import base64
 
-from rest import RestResource
-from models import Item
+from cryptography import fernet
+from aiohttp.web import Application, run_app
+from aiohttp_session import setup, get_session
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+
+from rest import (
+    RestResource,
+    ItemListEndpoint,
+    ItemDetailEndpoint,
+    AuthenticateEndpoint,
+)
+from models import Item, User
+from middlewares import authenticate_middleware
 
 app = Application()
-item_resource = RestResource('items', Item)
+fernet_key = fernet.Fernet.generate_key()
+secret_key = base64.urlsafe_b64decode(fernet_key)
+
+setup(app, EncryptedCookieStorage(secret_key))
+app.middlewares.append(authenticate_middleware)
+
+item_resource = RestResource(
+    'items',
+    Item,
+    list_endpoint=ItemListEndpoint,
+    detail_endpoint=ItemDetailEndpoint
+)
 item_resource.register(app.router)
+
+auth_resource = RestResource(
+    'auth',
+    User,
+    common_endpoint=AuthenticateEndpoint
+)
+auth_resource.register(app.router)
 
 if __name__ == '__main__':
     run_app(app, host='localhost', port=8000)
--- rest.py
+++ rest.py
@@ -1,22 +1,18 @@
 import inspect
 import json
+import hashlib
 
 from aiohttp.web import Request, Response
 from aiohttp.web import get, post, put, delete
 from aiohttp.web import Application, run_app
 from aiohttp.web import HTTPMethodNotAllowed, HTTPBadRequest
 from aiohttp.web import UrlDispatcher
-from aiohttp.web import middleware
+from aiohttp_session import get_session
 from sqlalchemy import inspect as sql_inspect
 
 from models import User, Item, session
 
 DEFAULT_METHODS = ('GET', 'POST', 'PUT', 'DELETE')
-
-
-@middleware
-async def authenticate_middleware():
-    pass
 
 
 class RestEndpoint:
@@ -52,16 +48,24 @@ class ItemListEndpoint(RestEndpoint):
         super().__init__()
         self.resource = resource
 
-    async def get(self) -> Response:
-        data = json.dumps({
-            'items': [item.to_json() for item in session.query(Item)]
-        }).encode('utf-8')
+    async def get(self, request) -> Response:
+        print(dir(request.__dict__))
+        if 'user' in request:
+            user_id = request['user'].id
+            print(user_id)
+            data = json.dumps({
+                'items': [item.to_json() for item in session.query(Item).filter(Item.created_by == user_id)]
+            }).encode('utf-8')
 
-        return Response(
-            status=200,
-            body=data,
-            content_type='application/json',
-        )
+            return Response(
+                status=200,
+                body=data,
+                content_type='application/json',
+            )
+        else:
+            return Response(
+                status=401
+            )
 
     async def post(self, request) -> Response:
         data = await request.json()
@@ -83,8 +87,9 @@ class ItemDetailEndpoint(RestEndpoint):
         super().__init__()
         self.resource = resource
     
-    async def get(self, pk):
-        instance = session.query(Item).filter(Item.id == pk).first()
+    async def get(self, request, pk):
+        user_id = request.user.id
+        instance = session.query(Item).filter(Item.id == pk, Item.created_by == user_id).first()
         if not instance:
             return Response(
                 status=404,
@@ -138,14 +143,50 @@ class ItemDetailEndpoint(RestEndpoint):
         return Response(status=204)
 
 
+class AuthenticateEndpoint(RestEndpoint):
+    def __init__(self, resource):
+        super().__init__()
+        self.resource = resource
+
+    async def post(self, request):
+        data = await request.json()
+        email = data['email']
+        password = hashlib.sha256(data['password'].encode('utf-8')).hexdigest()
+
+        user = session.query(User).filter(User.email == email, User.password == password).first()
+
+        if user:
+            http_session = await get_session(request)
+            http_session['user_id'] = user.id
+            return Response(status=200)
+        else:
+            data = json.dumps({'error': 'Bad Authentication data'}).encode('utf-8')
+            return Response(status=404, body=data, content_type='application/json')
+
+
 class RestResource:
-    def __init__(self, verbose_name, factory):
+    def __init__(
+            self,
+            verbose_name,
+            factory,
+            list_endpoint=None,
+            detail_endpoint=None,
+            common_endpoint=None
+        ):
         self.verbose_name = verbose_name
         self.factory = factory
 
-        self.items_endpoint = ItemListEndpoint(self)
-        self.item_detail_endpoint = ItemDetailEndpoint(self)
+        if list_endpoint:
+            self.list_endpoint = list_endpoint(self)
+        if detail_endpoint:
+            self.detail_endpoint = detail_endpoint(self)
+        if common_endpoint:
+            self.common_endpoint = common_endpoint(self)
     
     def register(self, router: UrlDispatcher):
-        router.add_route('*', '/{}'.format(self.verbose_name), self.items_endpoint.dispatch)
-        router.add_route('*', '/{}/{{pk}}'.format(self.verbose_name), self.item_detail_endpoint.dispatch)
+        if hasattr(self, 'list_endpoint'):
+            router.add_route('*', '/{}'.format(self.verbose_name), self.list_endpoint.dispatch)
+        if hasattr(self, 'detail_endpoint'):
+            router.add_route('*', '/{}/{{pk}}'.format(self.verbose_name), self.detail_endpoint.dispatch)
+        if hasattr(self, 'common_endpoint'):
+            router.add_route('*', '/{}'.format(self.verbose_name), self.common_endpoint.dispatch)
